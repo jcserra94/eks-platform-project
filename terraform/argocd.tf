ce1c904dc56092d09fdd5033d4dbc8392b35eaf8
juan.serrano <serra102@cougars.csusm.edu>
2026-08-21 11:46:31 -0700

    feat: add gitops structure, sample guestbook app, and terraform pr validation pipeline

---
+++ terraform/argocd.tf
@@ -0,0 +1,21 @@
+﻿resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  version    = "7.7.0"
+
+  values = [jsonencode({
+    server = {
+      service = {
+        type = "LoadBalancer"
+      }
+    }
+  })]
+}
